f92325a83efc488995213a3eda5533a86bf82f39
Alonso Mallea <alonso.mallea.221b@gmail.com>
2021-11-20 15:06:49 -0300

    Initial Commit

---
+++ Components/Navbar.php
@@ -0,0 +1,82 @@
+<!-- Header -->
+<nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="Vista/Home.php">
+                <figure class="image is-48x48">
+                    <img src="../Images/Global/logo.jpg">
+                </figure>
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+                data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="Home.php">
+                    Inicio
+                </a>
+
+                <a class="navbar-item" href="Servicios.php">
+                    Servicios
+                </a>
+
+                <a class="navbar-item" href="Contacto.php">
+                    Contacto
+                </a>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <a href="mailto: jspropiedades2020@gmail.com">
+                        <figure class="image is-32x32">
+                            <img src="../Images/Icons/mail-outline.svg">
+                        </figure>
+                    </a>
+                </div>
+
+                <div class="navbar-item">
+                    <a href="https://wa.link/uhq317" target="_blank">
+                        <figure class="image is-32x32">
+                            <img src="../Images/Icons/logo-whatsapp.svg">
+                        </figure>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- fin header -->
+
+    <!-- NavBar JavaScript -->
+    <script>
+    document.addEventListener('DOMContentLoaded', () => {
+
+        // Get all " navbar-burger" elements const
+        $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'),
+            0); // Check if there
+        are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+
+            // Add a click event on each of them
+            $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
+
+                    // Get the target from the "data-target" attribute
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+
+                    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+
+                });
+            });
+        }
+
+    });
+    </script>
---
+++ Vista/Contacto.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <P>
+        <title>Contacto - Corredora de Propiedades Jessica ADM Asesorias SPA</title>
+    </p>
+
+    <!-- Bulma -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma-social@2/css/all.min.css">
+</head>
+
+<body>
+    <h1 class="title">Pagina Contacto</h1>
+
+    <a href="Home.php">
+        Inicio
+    </a>
+
+    <a href="Servicios.php">
+        Servicios
+    </a>
+
+    <a href="Contacto.php">
+        Contacto
+    </a>
+</body>
+
+</html>
