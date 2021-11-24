25b37ff418d8252c33f28117c6360f2798f30995
Sebastian Mella Silva <66895766+Sebastian1412@users.noreply.github.com>
2021-11-24 14:20:42 -0300

    commit

--- Vista/Servicios.php
+++ Vista/Servicios.php
@@ -295,8 +295,7 @@
     document.addEventListener('DOMContentLoaded', () => {
         // Get all " navbar-burger" elements const
         $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'),
-            0); // Check if there
-        are any navbar burgers
+            0); // Check if there are any navbar burgers
         if ($navbarBurgers.length > 0) {
             // Add a click event on each of them
             $navbarBurgers.forEach(el => {
